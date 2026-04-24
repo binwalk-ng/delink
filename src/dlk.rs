e4dfe54bdd9cc4e1e899aea674cd7f6d95f44200
impervius <63152795+impervius@users.noreply.github.com>
2026-04-24 23:42:55 +0300

    Reduce nesting by using let Some() else {} statements

--- src/dlk.rs
+++ src/dlk.rs
@@ -36,143 +36,113 @@ pub fn decrypt(encrypted_data: &[u8]) -> Result<Vec<u8>, DecryptError> {
                 let signature_size_end = signature_size_start + PAYLOAD_SIZE_LEN;
 
                 // Get the size of the first header's payload data (the firmware signature)
-                match encrypted_data.get(signature_size_start..signature_size_end) {
-                    None => {
-                        debug!("Failed to read signature size field");
+                let Some(signature_size_bytes) =
+                    encrypted_data.get(signature_size_start..signature_size_end)
+                else {
+                    debug!("Failed to read signature size field");
+                    return Err(DecryptError::Input);
+                };
+
+                let signature_size =
+                    u32::from_le_bytes(signature_size_bytes.try_into().unwrap()) as usize;
+                let next_header_offset = HEADER_SIZE + signature_size;
+
+                // Validate the payload header's magic bytes
+                let Some(header2_magic) = encrypted_data
+                    .get(next_header_offset..next_header_offset + ENCRYPTED_MAGIC.len())
+                else {
+                    debug!("Failed to read payload header magic bytes");
+                    return Err(DecryptError::Input);
+                };
+
+                if header2_magic == ENCRYPTED_MAGIC {
+                    // Calculate the offset of the block size field
+                    let block_size_start = next_header_offset + BLOCK_SIZE_OFFSET;
+                    let block_size_end = block_size_start + BLOCK_SIZE_LEN;
+
+                    // Calculate the offset of the total size field
+                    let total_size_start = next_header_offset + PAYLOAD_SIZE_OFFSET;
+                    let total_size_end = total_size_start + PAYLOAD_SIZE_LEN;
+
+                    // Get the block size field
+                    let Some(block_size_bytes) =
+                        encrypted_data.get(block_size_start..block_size_end)
+                    else {
+                        debug!("Failed to read data block size");
                         return Err(DecryptError::Input);
-                    }
-                    Some(signature_size_bytes) => {
-                        let signature_size =
-                            u32::from_le_bytes(signature_size_bytes.try_into().unwrap()) as usize;
-                        let next_header_offset = HEADER_SIZE + signature_size;
+                    };
 
-                        // Validate the payload header's magic bytes
-                        match encrypted_data
-                            .get(next_header_offset..next_header_offset + ENCRYPTED_MAGIC.len())
-                        {
-                            None => {
-                                debug!("Failed to read payload header magic bytes");
-                                return Err(DecryptError::Input);
+                    // Interpret block size field as little endian u32
+                    let block_size = (u32::from_le_bytes(block_size_bytes.try_into().unwrap())
+                        as usize)
+                        + BLOCK_PADDING_SIZE;
+
+                    // Get the total size field
+                    let Some(total_size_bytes) =
+                        encrypted_data.get(total_size_start..total_size_end)
+                    else {
+                        debug!("Failed to read encrypted data size");
+                        return Err(DecryptError::Input);
+                    };
+
+                    // Interpret total size field as little endian u32
+                    let total_data_size =
+                        u32::from_le_bytes(total_size_bytes.try_into().unwrap()) as usize;
+
+                    // Calculate the start and end offsets of the array of encrypted blocks
+                    let cipher_data_start = next_header_offset + HEADER_SIZE;
+                    let cipher_data_end = cipher_data_start + total_data_size;
+
+                    // Get the bytes of all encrypted blocks
+                    let Some(block_data) = encrypted_data.get(cipher_data_start..cipher_data_end)
+                    else {
+                        debug!(
+                            "Failed to read encrypted data [{:#X} - {:#X}]",
+                            cipher_data_start, cipher_data_end
+                        );
+                        return Err(DecryptError::Input);
+                    };
+
+                    // Loop through all known keys
+                    for (key, name) in known_keys().iter() {
+                        debug!("Trying {} key...", name);
+
+                        let mut decrypted_data: Vec<u8> = Vec::new();
+                        let mut processed_data_size: usize = 0;
+
+                        // Loop through each encrypted block
+                        for block in block_data.chunks(block_size) {
+                            // A block consists of a 16-byte IV followed by the encrypted data;
+                            // hence a valid block must have more than 16 bytes in it.
+                            if block.len() <= IV_SIZE {
+                                warn!("Block is too small!");
+                                break;
                             }
-                            Some(header2_magic) => {
-                                if header2_magic == ENCRYPTED_MAGIC {
-                                    // Calculate the offset of the block size field
-                                    let block_size_start = next_header_offset + BLOCK_SIZE_OFFSET;
-                                    let block_size_end = block_size_start + BLOCK_SIZE_LEN;
 
-                                    // Calculate the offset of the total size field
-                                    let total_size_start = next_header_offset + PAYLOAD_SIZE_OFFSET;
-                                    let total_size_end = total_size_start + PAYLOAD_SIZE_LEN;
+                            // Separate out the IV from the encrypted block data
+                            let iv = &block[0..IV_SIZE];
+                            let encrypted_block = &block[IV_SIZE..];
 
-                                    // Get the block size field
-                                    match encrypted_data.get(block_size_start..block_size_end) {
-                                        None => {
-                                            debug!("Failed to read data block size");
-                                            return Err(DecryptError::Input);
-                                        }
-                                        Some(block_size_bytes) => {
-                                            // Interpret block size field as little endian u32
-                                            let block_size = (u32::from_le_bytes(
-                                                block_size_bytes.try_into().unwrap(),
-                                            )
-                                                as usize)
-                                                + BLOCK_PADDING_SIZE;
-
-                                            // Get the total size field
-                                            match encrypted_data
-                                                .get(total_size_start..total_size_end)
-                                            {
-                                                None => {
-                                                    debug!("Failed to read encrypted data size");
-                                                    return Err(DecryptError::Input);
-                                                }
-                                                Some(total_size_bytes) => {
-                                                    // Interpret total size field as little endian u32
-                                                    let total_data_size = u32::from_le_bytes(
-                                                        total_size_bytes.try_into().unwrap(),
-                                                    )
-                                                        as usize;
-
-                                                    // Calculate the start and end offsets of the array of encrypted blocks
-                                                    let cipher_data_start =
-                                                        next_header_offset + HEADER_SIZE;
-                                                    let cipher_data_end =
-                                                        cipher_data_start + total_data_size;
-
-                                                    // Get the bytes of all encrypted blocks
-                                                    match encrypted_data
-                                                        .get(cipher_data_start..cipher_data_end)
-                                                    {
-                                                        None => {
-                                                            debug!(
-                                                                "Failed to read encrypted data [{:#X} - {:#X}]",
-                                                                cipher_data_start, cipher_data_end
-                                                            );
-                                                            return Err(DecryptError::Input);
-                                                        }
-                                                        Some(block_data) => {
-                                                            // Loop through all known keys
-                                                            for (key, name) in known_keys().iter() {
-                                                                debug!("Trying {} key...", name);
-
-                                                                let mut decrypted_data: Vec<u8> =
-                                                                    Vec::new();
-                                                                let mut processed_data_size: usize =
-                                                                    0;
-
-                                                                // Loop through each encrypted block
-                                                                for block in
-                                                                    block_data.chunks(block_size)
-                                                                {
-                                                                    // A block consists of a 16-byte IV followed by the encrypted data;
-                                                                    // hence a valid block must have more than 16 bytes in it.
-                                                                    if block.len() <= IV_SIZE {
-                                                                        warn!(
-                                                                            "Block is too small!"
-                                                                        );
-                                                                        break;
-                                                                    }
-
-                                                                    // Separate out the IV from the encrypted block data
-                                                                    let iv = &block[0..IV_SIZE];
-                                                                    let encrypted_block =
-                                                                        &block[IV_SIZE..];
-
-                                                                    // Decrypt the block
-                                                                    match aes_256_cbc_decrypt(
-                                                                        encrypted_block,
-                                                                        &key.clone().into_bytes(),
-                                                                        iv,
-                                                                    ) {
-                                                                        Err(_) => {
-                                                                            break;
-                                                                        }
-                                                                        Ok(decrypted_block) => {
-                                                                            // Track how much data has been successfully decrypted and add the decrypted data to decrypted_data
-                                                                            processed_data_size +=
-                                                                                block.len();
-                                                                            decrypted_data.extend(
-                                                                                decrypted_block,
-                                                                            );
-                                                                        }
-                                                                    }
-                                                                }
-
-                                                                // If all data was decrypted successfully, return success
-                                                                if processed_data_size
-                                                                    == total_data_size
-                                                                {
-                                                                    return Ok(decrypted_data);
-                                                                }
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
+                            // Decrypt the block
+                            match aes_256_cbc_decrypt(
+                                encrypted_block,
+                                &key.clone().into_bytes(),
+                                iv,
+                            ) {
+                                Err(_) => {
+                                    break;
+                                }
+                                Ok(decrypted_block) => {
+                                    // Track how much data has been successfully decrypted and add the decrypted data to decrypted_data
+                                    processed_data_size += block.len();
+                                    decrypted_data.extend(decrypted_block);
                                 }
                             }
+                        }
+
+                        // If all data was decrypted successfully, return success
+                        if processed_data_size == total_data_size {
+                            return Ok(decrypted_data);
                         }
                     }
                 }
