5a7dbc85ae383088217fa1da023d9dbd77b8495c
impervius <63152795+impervius@users.noreply.github.com>
2026-05-29 21:36:50 +0300

    Migrate to new rustcrypto versions and update dependencies

--- src/aes.rs
+++ src/aes.rs
@@ -1,19 +1,14 @@
 use crate::common::DecryptError;
-use aes::cipher::{BlockCipher, BlockDecrypt, BlockDecryptMut, KeyIvInit, block_padding::Pkcs7};
+use aes::cipher::{
+    BlockModeDecrypt, KeyIvInit,
+    block_padding::{NoPadding, Pkcs7},
+};
 use log::warn;
 
 pub enum AesKeySize {
     AES128 = 16,
     _AES192 = 24,
     AES256 = 32,
-}
-
-/// Supported AES variants for decryption
-#[derive(Debug)]
-enum AesVariant {
-    Aes128(cbc::Decryptor<aes::Aes128>),
-    Aes192(cbc::Decryptor<aes::Aes192>),
-    Aes256(cbc::Decryptor<aes::Aes256>),
 }
 
 /// Padding modes for AES decryption
@@ -25,107 +20,39 @@ pub enum AesPaddingMode {
     WithPadding,
 }
 
-impl AesVariant {
-    /// Creates a new AES variant with the specified key size
-    fn new(key: &[u8], iv: &[u8], key_size: AesKeySize) -> Result<Self, DecryptError> {
-        let key_size = key_size as usize;
-        let cropped_key = key
-            .get(..key_size)
-            .ok_or(DecryptError::InvalidKeySize(key.len()))?;
-
-        match key_size {
-            16 => Ok(AesVariant::Aes128(cbc::Decryptor::new(
-                cropped_key.into(),
-                iv.into(),
-            ))),
-            24 => Ok(AesVariant::Aes192(cbc::Decryptor::new(
-                cropped_key.into(),
-                iv.into(),
-            ))),
-            32 => Ok(AesVariant::Aes256(cbc::Decryptor::new(
-                cropped_key.into(),
-                iv.into(),
-            ))),
-            size => Err(DecryptError::InvalidKeySize(size)),
-        }
+/// Decrypts data without padding using an initialized decryptor
+fn decrypt_unpadded<M: BlockModeDecrypt>(
+    decryptor: M,
+    encrypted_data: &[u8],
+) -> Result<Vec<u8>, DecryptError> {
+    if encrypted_data.len() % 16 != 0 {
+        return Err(DecryptError::InvalidInputLength);
     }
 
-    /// Decrypts data without padding
-    fn decrypt_unpadded<C: BlockCipher + BlockDecrypt>(
-        mut decryptor: cbc::Decryptor<C>,
-        encrypted_data: &[u8],
-    ) -> Result<Vec<u8>, DecryptError> {
-        if !encrypted_data.len().is_multiple_of(C::block_size()) {
-            return Err(DecryptError::InvalidInputLength);
-        }
+    let mut output_buffer = encrypted_data.to_vec();
+    decryptor
+        .decrypt_padded::<NoPadding>(&mut output_buffer)
+        .map_err(|_| DecryptError::InvalidInputLength)?;
 
-        let mut output_buffer = vec![0; encrypted_data.len()];
-        for (chunk, output) in encrypted_data
-            .chunks(C::block_size())
-            .zip(output_buffer.chunks_mut(C::block_size()))
-        {
-            decryptor.decrypt_block_b2b_mut(chunk.into(), output.into());
-        }
-        Ok(output_buffer)
-    }
+    Ok(output_buffer)
+}
 
-    /// Decrypts data with PKCS7 padding
-    fn decrypt_padded<C: BlockCipher + BlockDecryptMut>(
-        decryptor: cbc::Decryptor<C>,
-        encrypted_data: &[u8],
-    ) -> Result<Vec<u8>, DecryptError> {
-        let mut decrypted_buf = vec![0; encrypted_data.len()];
-        decryptor
-            .decrypt_padded_b2b_mut::<Pkcs7>(encrypted_data, &mut decrypted_buf)
-            .map(|data| data.to_vec())
-            .map_err(|_| {
-                warn!("Decryption failed with padding");
-                DecryptError::Decrypt
-            })
-    }
-
-    /// Decrypts data using the appropriate method based on padding mode
-    fn decrypt(
-        &self,
-        encrypted_data: &[u8],
-        padding: AesPaddingMode,
-    ) -> Result<Vec<u8>, DecryptError> {
-        match (self, padding) {
-            (AesVariant::Aes128(decryptor), AesPaddingMode::NoPadding) => {
-                Self::decrypt_unpadded(decryptor.clone(), encrypted_data)
-            }
-            (AesVariant::Aes128(decryptor), AesPaddingMode::WithPadding) => {
-                Self::decrypt_padded(decryptor.clone(), encrypted_data)
-            }
-            (AesVariant::Aes192(decryptor), AesPaddingMode::NoPadding) => {
-                Self::decrypt_unpadded(decryptor.clone(), encrypted_data)
-            }
-            (AesVariant::Aes192(decryptor), AesPaddingMode::WithPadding) => {
-                Self::decrypt_padded(decryptor.clone(), encrypted_data)
-            }
-            (AesVariant::Aes256(decryptor), AesPaddingMode::NoPadding) => {
-                Self::decrypt_unpadded(decryptor.clone(), encrypted_data)
-            }
-            (AesVariant::Aes256(decryptor), AesPaddingMode::WithPadding) => {
-                Self::decrypt_padded(decryptor.clone(), encrypted_data)
-            }
-        }
-    }
+/// Decrypts data with PKCS7 padding using an initialized decryptor
+fn decrypt_padded<M: BlockModeDecrypt>(
+    decryptor: M,
+    encrypted_data: &[u8],
+) -> Result<Vec<u8>, DecryptError> {
+    let mut output_buffer = encrypted_data.to_vec();
+    decryptor
+        .decrypt_padded::<Pkcs7>(&mut output_buffer)
+        .map(|data| data.to_vec())
+        .map_err(|_| {
+            warn!("Decryption failed with padding");
+            DecryptError::Decrypt
+        })
 }
 
 /// Generic AES-CBC decryption function
-///
-/// # Arguments
-///
-/// * `encrypted_data` - The data to decrypt
-/// * `key` - The encryption key
-/// * `iv` - The initialization vector
-/// * `key_size` - Size of the key in bytes (16 for AES-128, 32 for AES-256)
-/// * `padding` - The padding mode to use
-///
-/// # Returns
-///
-/// The decrypted data or a DecryptError
 pub fn aes_cbc_decrypt(
     encrypted_data: &[u8],
     key: &[u8],
@@ -133,8 +60,45 @@ pub fn aes_cbc_decrypt(
     key_size: AesKeySize,
     padding: AesPaddingMode,
 ) -> Result<Vec<u8>, DecryptError> {
-    let decryptor = AesVariant::new(key, iv, key_size)?;
-    decryptor.decrypt(encrypted_data, padding)
+    let key_len = key_size as usize;
+    let cropped_key = key
+        .get(..key_len)
+        .ok_or(DecryptError::InvalidKeySize(key.len()))?;
+
+    // IV for AES is universally 16 bytes.
+    let iv_bytes: &[u8; 16] = iv
+        .get(..16)
+        .and_then(|slice| slice.try_into().ok())
+        .ok_or(DecryptError::InvalidInputLength)?;
+
+    // Instantiate the decryptor engine and immediately process the data
+    match key_len {
+        16 => {
+            let key_bytes: &[u8; 16] = cropped_key.try_into().map_err(|_| DecryptError::Decrypt)?;
+            let decryptor = cbc::Decryptor::<aes::Aes128>::new(key_bytes.into(), iv_bytes.into());
+            match padding {
+                AesPaddingMode::NoPadding => decrypt_unpadded(decryptor, encrypted_data),
+                AesPaddingMode::WithPadding => decrypt_padded(decryptor, encrypted_data),
+            }
+        }
+        24 => {
+            let key_bytes: &[u8; 24] = cropped_key.try_into().map_err(|_| DecryptError::Decrypt)?;
+            let decryptor = cbc::Decryptor::<aes::Aes192>::new(key_bytes.into(), iv_bytes.into());
+            match padding {
+                AesPaddingMode::NoPadding => decrypt_unpadded(decryptor, encrypted_data),
+                AesPaddingMode::WithPadding => decrypt_padded(decryptor, encrypted_data),
+            }
+        }
+        32 => {
+            let key_bytes: &[u8; 32] = cropped_key.try_into().map_err(|_| DecryptError::Decrypt)?;
+            let decryptor = cbc::Decryptor::<aes::Aes256>::new(key_bytes.into(), iv_bytes.into());
+            match padding {
+                AesPaddingMode::NoPadding => decrypt_unpadded(decryptor, encrypted_data),
+                AesPaddingMode::WithPadding => decrypt_padded(decryptor, encrypted_data),
+            }
+        }
+        size => Err(DecryptError::InvalidKeySize(size)),
+    }
 }
 
 /// Convenience function for AES-128-CBC decryption with padding
--- src/sha1_hmac.rs
+++ src/sha1_hmac.rs
@@ -1,9 +1,9 @@
-use hmac::{Hmac, Mac};
+use hmac::{KeyInit, Mac, SimpleHmac};
 use sha1::Sha1;
 
 /// Generates a SHA1 from an HMAC key and message
 pub fn sha1_hmac(key: &[u8], message: &[u8]) -> Vec<u8> {
-    let mut hasher: Hmac<Sha1> = Mac::new_from_slice(key).expect("Failed to instantiate Mac");
+    let mut hasher = SimpleHmac::<Sha1>::new_from_slice(key).expect("Failed to instantiate Mac");
     hasher.update(message);
     hasher.finalize().into_bytes().to_vec()
 }
